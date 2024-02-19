27c79f3b68d230bcb612afc1006e81d6c022031f
Aduilio Silva <developer.aduilio@gmail.com>
2024-02-19 20:20:31 -0300

    Adjust logs

--- src/main/kotlin/com/aduilio/tasks/service/TaskService.kt
+++ src/main/kotlin/com/aduilio/tasks/service/TaskService.kt
@@ -34,12 +34,12 @@ class TaskService(
      * @return ReadTaskDto
      */
     fun create(createTaskDto: CreateTaskDto): ReadTaskDto {
-        logger.debug { "Creating a task: $createTaskDto"}
+        logger.debug { "Creating a task: $createTaskDto" }
 
         val task = taskMapper.mapTaskFrom(createTaskDto)
         val result = taskRepository.save(task)
 
-        logger.debug { "Task created with id: ${result.id}"}
+        logger.debug { "Task created with id: ${result.id}" }
 
         return taskMapper.mapReadTaskDtoFrom(result)
     }
@@ -54,11 +54,11 @@ class TaskService(
      * @exception TaskCompleteException if the task is already completed
      */
     fun update(id: Long, updateTaskDto: UpdateTaskDto): ReadTaskDto {
-        logger.debug { "Updating the task with id: $id"}
+        logger.debug { "Updating the task with id: $id and payload $updateTaskDto" }
 
         val task = readTask(id)
         if (task.completed) {
-            logger.debug { "Update not allowed. Task is completed."}
+            logger.debug { "Update not allowed. Task is completed." }
 
             throw TaskCompleteException(TASK_ALREADY_COMPLETED)
         }
@@ -66,7 +66,7 @@ class TaskService(
         val updateTask = taskMapper.mapTaskFrom(updateTaskDto, task)
         taskRepository.save(updateTask)
 
-        logger.debug { "Task with id: $id updated"}
+        logger.debug { "Task with id: $id updated" }
 
         return taskMapper.mapReadTaskDtoFrom(updateTask)
     }
@@ -77,11 +77,11 @@ class TaskService(
      * @return List of ReadTaskDto
      */
     fun list(): List<ReadTaskDto> {
-        logger.debug { "Listing all tasks"}
+        logger.debug { "Listing all tasks" }
 
         val tasks = taskRepository.findAllByOrderByIdDesc()
 
-        logger.debug { "Found ${tasks.size} tasks"}
+        logger.debug { "Found ${tasks.size} tasks" }
 
         return tasks.map { task -> taskMapper.mapReadTaskDtoFrom(task) }
     }
@@ -94,11 +94,11 @@ class TaskService(
      * @exception NotFoundException if the id is invalid
      */
     fun read(id: Long): ReadTaskDto {
-        logger.debug { "Reading the task with id: $id"}
+        logger.debug { "Reading the task with id: $id" }
 
         val task = readTask(id)
 
-        logger.debug { "Task: $task"}
+        logger.debug { "Task: $task" }
 
         return taskMapper.mapReadTaskDtoFrom(task)
     }
@@ -110,11 +110,11 @@ class TaskService(
      * @exception NotFoundException if the id is invalid
      */
     fun delete(id: Long) {
-        logger.debug { "Deleting the task with id: $id"}
+        logger.debug { "Deleting the task with id: $id" }
 
         taskRepository.deleteById(id)
 
-        logger.debug { "Task with id: $id deleted"}
+        logger.debug { "Task with id: $id deleted" }
     }
 
     /**
@@ -124,12 +124,12 @@ class TaskService(
      * @exception NotFoundException if the id is invalid
      */
     fun complete(id: Long): ReadTaskDto {
-        logger.debug { "Completing the task with id: $id"}
+        logger.debug { "Completing the task with id: $id" }
 
         val task = readTask(id).copy(completed = true)
         taskRepository.save(task)
 
-        logger.debug { "Task with id: $id completed"}
+        logger.debug { "Task with id: $id completed" }
 
         return taskMapper.mapReadTaskDtoFrom(task)
     }
@@ -138,11 +138,11 @@ class TaskService(
      * Deletes all completed tasks.
      */
     fun deleteCompletedTasks() {
-        logger.debug { "Deleting all completed tasks"}
+        logger.debug { "Deleting all completed tasks" }
 
         taskRepository.deleteByCompleted()
 
-        logger.debug { "All completed tasks deleted"}
+        logger.debug { "All completed tasks deleted" }
     }
 
     private fun readTask(id: Long): Task {
